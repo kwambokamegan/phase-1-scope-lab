d6ced8f85c6fe3b0ab5c1e308019772bd49319aa
Megan <megan.nyakina@student.moringaschool.com>
2024-06-26 20:08:44 +0300

    aadds scope lab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution in this file!
+//customer name 
+
+var customerName = "bob";
+
+
+//upperCaseCustomerName
+  
+function upperCaseCustomerName() {
+
+customerName = "BOB";
+
+}
+
+//setBestCustomer
+
+function setBestCustomer(){
+
+   bestCustomer = "not bob";
+}
+
+
+//overwriteBestCustomer
+
+function overwriteBestCustomer () {
+
+    bestCustomer = "maybe bob";
+}
+
+
+//leastFavouriteCustomer
+ 
+const leastFavouriteCustomer = 'bob';
+
+
+
+//changeLeastFavouriteCustomer
+
+function changeLeastFavouriteCustomer () {
+
+    leastFavouriteCustomer = ' not bob';
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
