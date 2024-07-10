7350724c9f0eac54ffcb98842fb5bdf748bcf06f
Megan <megan.nyakina@student.moringaschool.com>
2024-07-11 00:04:17 +0300

    adds scope lab

--- index.js
+++ index.js
@@ -1,57 +1,37 @@
-//customer name 
+// Global variable declared with var
+var customerName = 'bob';
 
-var customerName = "bob";
-
-
-//upperCaseCustomerName
-  
+// Function to uppercase customerName
 function upperCaseCustomerName() {
-
-customerName = "BOB";
-
+  customerName = customerName.toUpperCase();
 }
 
-//setBestCustomer
-
-function setBestCustomer(){
-
-   bestCustomer = "not bob";
+// Function that declares a global variable bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Note: using var to declare a global variable inside a function (as per lab instructions)
 }
 
-
-//overwriteBestCustomer
-
-function overwriteBestCustomer () {
-
-    bestCustomer = "maybe bob";
+// Function that overwrites the global variable bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
 }
 
-
-//leastFavouriteCustomer
- 
+// Constant declared in global scope
 const leastFavouriteCustomer = 'bob';
 
-
-
-//changeLeastFavouriteCustomer
-
-function changeLeastFavouriteCustomer () {
-
-    leastFavouriteCustomer = ' not bob';
-    
+// Function that attempts to change the constant leastFavouriteCustomer
+function changeLeastFavouriteCustomer() {
+  // Attempting to change a constant will throw an error or be ignored (in non-strict mode)
+  // This is to demonstrate the error as per the lab instructions
+  leastFavouriteCustomer = 'not bob'; // This will cause an error when executed
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Exporting functions for testing purposes if required
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavouriteCustomer,
+  changeLeastFavouriteCustomer // Corrected function name to match the defined function
+};
